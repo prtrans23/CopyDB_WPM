bf7cb51daff8b60aaf0e9336113f996999cd0e44
prtrans23 <prtrans23@gmail.com>
2018-11-19 13:27:04 -0800

    BackUp3
    Add Resource Reader, Finder

---
+++ CopyDB_WPM/Util/ReadFile/ResourceFinder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyDB_WPM.Util.ReadFile
+{
+    class ResourceFinder
+    {
+        const string mssql = "TSQL";
+        const string mysql = "YSQL";
+
+        public static string Get_TSQL_Path(string section, string FileName)
+        {
+            return $"{GetResourceDir(mssql)}\\{section}\\{FileName}";
+        }
+
+        public static string Get_YSQL_Path(string section, string FileName)
+        {
+            return $"{GetResourceDir(mysql)}\\{section}\\{FileName}";
+        }
+
+        public static string GetResourceDir(string targetDocument)
+        {
+            string folderPath = AppDomain.CurrentDomain.BaseDirectory;
+
+            switch (targetDocument)
+            {
+                case mssql: // mssql
+                    folderPath += "\\Resource\\Script\\TSQL\\";
+                    break;
+                case mysql: // mysql
+                    folderPath += "\\Resource\\Script\\YSQL\\";
+                    break;
+            }
+
+            return folderPath;
+        }
+
+
+    }
+}
---
+++ CopyDB_WPM/Util/ReadFile/ResourceReader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyDB_WPM.Util.ReadFile
+{
+    class ResourceReader
+    {
+        public static string ReadSqlFileToStr(string fileName)
+        {
+            //var value = Properties.Resources.ResourceManager.GetObject(fileName, Properties.Resources.Culture);
+
+            var type = typeof(Properties.Resources);
+            var property = type.GetProperty(fileName, BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
+            var value = property.GetValue(null, null);
+
+            return (string)value;
+        }
+    }
+}
